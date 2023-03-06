ebe8c64a2aa47ea6a55fcee981451ea46ac9e87b
LETICIA FERNANDA PEREIRA <leticia.6978@aluno.pr.senac.br>
2023-03-06 08:41:22 -0300

    php

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    $nome="Letícia Fernanda";
+    $idade=16;
+    $endereco="Avenida Tancredo Neves 725";
+    $cidade="Ivaiporã";
+    $cep=86870-000;
+    $primeirosemestre=8;
+    $segundosemestre=10;
+    $terceirosemestre=9;
+    $media=($primeirosemestre+$segundosemestre+$terceirosemestre)/3;
+
+        echo "O seu nome é: " . $nome;
+        echo("</br>");
+        echo "Sua idade é: " . $idade;
+        echo("</br>");
+        echo "Seu endereço é: " . $endereco;
+        echo("</br>");
+        echo "Sua cidade é: " . $cidade;
+        echo("</br>");
+        echo "Seu cep é: " . $cep;
+        echo("</br>");
+        echo "Seu primeiro semestre é: " . $primeirosemestre;
+        echo("</br>");
+        echo "Seu segundo semestre é: " . $segundosemestre;
+        echo("</br>");
+        echo "Seu terceiro semestre é: " . $terceirosemestre;
+        echo("</br>");
+        echo "Sua média é: " . $media;
+
+
+?>
